536e9e17b058455b59c60f28cc60bf0b7dd5fa2e
Roo-kjh <wldnro3141@gmail.com>
2021-04-05 13:28:12 +0900

    make settings

---
+++ til_db_updater/settings.py
@@ -0,0 +1,12 @@
+# Settings
+
+# REPO
+REPO_URL = "https://github.com/Root-kjh/til"
+REPO_DIR = "/home/test/TIL"
+
+#DB
+DB_CONNECTION = {
+    "server": "localhost",
+    "port": "9200",
+    "index": "TIL"
+}
